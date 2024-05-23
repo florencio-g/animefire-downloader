b7b337b63cd488fc321016f1bf2410b150d9aef8
florenciogoncalves <florencio.goncalves06.com>
2024-05-23 14:56:39 +0100

    Add "ever download at better quality"

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 > Deseja baixar o seu anime completo ou um intervalo de episódios desejados? Tens aqui a sua solução!<br>O AnimefireDownloader faz por sí o download completo do seu anime podendo acompanhar todo o percurso.
 
 ## Future Features
+* Implementação de Interface gráfica(front-end)
 * Baixar lista de episódios específicos
 * Baixar vários animes na sequência
 * Optar por baixar sempre na melhor qualidade
--- app.js
+++ app.js
@@ -9,30 +9,45 @@ async function main() {
         FileManager.createFolderIfNotExist(`${__dirname}/downloads/${downloadingInfo.name}`)
 
         for (let index = downloadingInfo.start; index <= downloadingInfo.end; index++) {
-            var url = `https://lightspeedst.net/s2/mp4/one-piece/sd/${index}.mp4`
-            var path = `./downloads/${downloadingInfo.name}/${downloadingInfo.name} - Episódio ${index < 10 ? '0' + index : index}.mp4`
+            var url = downloadQuality => `https://lightspeedst.net/s2/mp4/${downloadingInfo.slug}/${downloadQuality}/${index}.mp4`
+            var path = downloadQuality => `./downloads/${downloadingInfo.name}/${downloadingInfo.name} - EP ${index < 10 ? '0' + index : index} (${downloadQuality}).mp4`
             var name = downloadingInfo.name + ' EP ' + index + ' -'
 
-            await FileManager.downloadFile(url, path, name)
+            await FileManager.downloadFile(url('fhd'), path('F-HD'), name)
                 .then(() => {
                     console.log('Download concluído!')
                     downloadingInfo.start += 1
                     FileManager.writeFile(__dirname, 'downloading-info.json', JSON.stringify(downloadingInfo))
                 })
-                .catch(() => {
-                    console.clear()
-                    console.log(`Não foi possível baixar o EP ${index} de ${downloadingInfo.name}.`)
-                    downloadingInfo.start += 1
-                    downloadingInfo.noDownloaded.push(index)
-                    downloadingInfo.noDownloaded.includes(index) ? downloadingInfo : downloadingInfo.noDownloaded.push(index)
-                    FileManager.writeFile(__dirname, 'downloading-info.json', JSON.stringify(downloadingInfo))
+                .catch(async () => {
+                    await FileManager.downloadFile(url('hd'), path('HD'), name)
+                        .then(() => {
+                            console.log('Download concluído!')
+                            downloadingInfo.start += 1
+                            FileManager.writeFile(__dirname, 'downloading-info.json', JSON.stringify(downloadingInfo))
+                        })
+                        .catch(async () => {
+                            await FileManager.downloadFile(url('sd'), path('SD'), name)
+                                .then(() => {
+                                    console.log('Download concluído!')
+                                    downloadingInfo.start += 1
+                                    FileManager.writeFile(__dirname, 'downloading-info.json', JSON.stringify(downloadingInfo))
+                                })
+                                .catch(() => {
+                                    console.clear()
+                                    console.log(`Não foi possível baixar o EP ${index} de ${downloadingInfo.name}.`)
+                                    downloadingInfo.start += 1
+                                    downloadingInfo.noDownloaded.includes(index) ? downloadingInfo : downloadingInfo.noDownloaded.push(index)
+                                    FileManager.writeFile(__dirname, 'downloading-info.json', JSON.stringify(downloadingInfo))
+                                })
+                        })
                 });
         }
     }
 }
 
 app.listen(3000, (error) => {
-    if(!error) {
+    if (!error) {
         main()
     }
 })
